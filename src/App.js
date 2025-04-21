c4d6dbfb2afe0eb9b30297eaf55422a5cbb4590f
AmmarLokhandwala <ammarabbas652@gmail.com>
2025-04-21 16:59:53 +0500

    Add all tasks related pages

--- src/App.js
+++ src/App.js
@@ -32,6 +32,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Loginpage";
 import RegisterPage from "./pages/Registerpage";
+import TaskListPage from "./pages/TaskListpage";  
+import TaskDetailPage from "./pages/TaskDetailpage";
+import NewTaskPage from "./pages/NewTaskpage";
+import AdminDashboard from "./pages/AdminDashboard";
+import UserProfile from "./components/User/UserProfile"; // Import the UserProfile component
+
 
 function App() {
   return (
@@ -39,6 +45,11 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path = "/tasks" element={<TaskListPage />} />
+        <Route path = "/new-task" element={<NewTaskPage />} />
+        <Route path = "/tasks/:taskId" element={<TaskDetailPage />} />
+        <Route path = "/dashboard" element={<AdminDashboard />} />
+        <Route path = "/profile" element={<UserProfile />} /> {/* User Profile route */}
         {/* Future routes: Dashboard, Task List, etc. */}
         <Route path="/" element={<LoginPage />} />
       </Routes>
